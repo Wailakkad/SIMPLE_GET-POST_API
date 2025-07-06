720a57618a3ab096c15b3314ed61e17286cace1a
Wailakkad <akkadouail8@gmail.com>
2025-07-06 16:21:56 +0200

    change file name

--- api.js
+++ api.js
@@ -1,24 +1,23 @@
+// api.js
+
 const express = require('express');
 const cors = require('cors');
-const port = 3000;
+const serverless = require('serverless-http'); 
+const dotenv = require('dotenv');
 const { connectDB } = require('./DataConfig/db');
 const routes = require('./routes/Route');
-dotenv = require('dotenv');
+
 dotenv.config();
-connectDB();
+
+connectDB(); // Connect to MongoDB Atlas
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Define the base route for the API
-// This sets up the API to handle requests at the '/api/v1/items' endpoint.
+// Your API routes
 app.use('/api/v1/items', routes);
 
 
-
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+module.exports.handler = serverless(app);
